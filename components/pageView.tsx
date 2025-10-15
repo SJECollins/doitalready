2ecab9bca1e92634e02a59ee5751c1f3c051df9a
SJECollins <66878688+SJECollins@users.noreply.github.com>
2025-10-15 09:07:10 +0100

    Chaneg PageView to simple View

--- components/pageView.tsx
+++ components/pageView.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from "react";
-import { ScrollView } from "react-native";
+import { View } from "react-native";
 import { useTheme } from "react-native-paper";
 
 export default function PageView({ children }: { children: ReactNode }) {
   const theme = useTheme();
 
   return (
-    <ScrollView
-      contentContainerStyle={{
+    <View
+      style={{
         flexGrow: 1,
         // alignItems: "center",
         padding: 20,
@@ -15,6 +15,6 @@ export default function PageView({ children }: { children: ReactNode }) {
       }}
     >
       {children}
-    </ScrollView>
+    </View>
   );
 }
